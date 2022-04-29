9b6431143e59fc8c01d581830871b71eb4994329
TLTL <103755573+TLTL1209@users.noreply.github.com>
2022-04-29 23:57:03 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,72 @@
+const quiz = [
+  {
+    question:'ゲーム史上、最も売れたゲーム機は次のうちどれ？',
+    answer: [ 'スーパーファミコン',
+    'プレイステーション２',
+    'ニンテンドースイッチ',
+    'ニンテンドーDS'
+  ],
+  correct:'ニンテンドーDS'
+  },
+  {
+    question:'糸井重里が企画に関わった、任天堂の看板ゲームといえば？',
+    answer:[ 'MOTHER2',
+    'スーパーマリオブラザース３',
+    'スーパードンキーコング',
+    '星のカービィ'
+  ],
+  correct:'MOTHER2'
+  },
+  {
+    question:'ファイナルファンタジーⅣの主人公の名前は？',
+    answer:[ 'フリオニール',
+    'クラウド',
+    'ティーダ',
+    'セシル'
+  ],
+  correct:'セシル'
+  },
+]
+
+const quizLength = quiz.length;
+let quizIndex = 0;
+let score = 0;
+
+
+const $button = document.getElementsByTagName('button');
+const buttonlength = $button.length;
+
+const setupQuiz = () => {
+  document.getElementById('js-question').textContent = quiz[quizIndex]. question;
+  let buttonIndex = 0;
+while(buttonIndex < buttonlength){
+  $button[buttonIndex].textContent = quiz[quizIndex].answer[buttonIndex];
+buttonIndex++;
+}
+}
+setupQuiz();
+
+const clickHandler = (e) => {
+  if(quiz[quizIndex].correct === e.target.textContent){
+    window.alert('正解！');
+    score++;
+  }else{
+    window.alert('不正解！');
+  }
+
+  quizIndex++;
+
+  if(quizIndex < quizLength){
+setupQuiz();
+  }else{
+    window.alert('終了！あなたの正解数は' + score + '/' + quizLength + 'です！');
+  }
+};
+
+let handolerIndex = 0;
+while (handolerIndex < buttonlength) {
+  $button[handolerIndex].addEventListener('click', (e) =>{
+    clickHandler(e);
+  });
+handolerIndex++;
+}
